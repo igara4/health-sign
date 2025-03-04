a2581784a2d3e063be976c00bedbb2ba79fb0e79
igara4 <fifty.storm.snowman@gmail.com>
2025-03-05 08:14:59 +0900

    ヘッダーレイアウト作成

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -2,6 +2,7 @@
 
 import { createClient } from "@/utils/supabase/client";
 import { User } from "@supabase/supabase-js";
+import { LogOut } from "lucide-react";
 import Link from "next/link"
 import { useRouter } from "next/navigation";
 
@@ -31,6 +32,28 @@ const Navigation = ({user}:NavigationProps) => {
           Health Sign
         </Link>
       </div>
+      <div className="text-sm font-bold">
+          {user ? (
+            <div className="flex items-center space-x-5">
+              <Link href="/conditions/new">
+                <div>記録する</div>
+              </Link>
+
+              <Link href="/settings/profile">
+                <div>設定</div>
+              </Link>
+
+              <div className="cursor-pointer" onClick={handleLogout}>
+                <LogOut className="h-5 w-5" />
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center space-x-5">
+              <Link href="/login">ログイン</Link>
+              <Link href="/signup">サインアップ</Link>
+            </div>
+          )}
+        </div>
     </header>
   )
 }
