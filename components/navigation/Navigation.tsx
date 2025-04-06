3e96afadf979d2c1749ca7c110b0a48c1afead0a
igara4 <fifty.storm.snowman@gmail.com>
2025-04-07 07:12:49 +0900

    ヘッダーを調整

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -27,32 +27,33 @@ const Navigation = ({user}:NavigationProps) => {
     }
   return (
     <header className="border-b">
-      <div className="mx-auto max-w-screen-lg px-2 py-5 flex items-center justify-between">
-        <Link href="/" className="font-bold text-xl">
+      <div className="mx-auto max-w-screen-lg px-2 py-5">
+        <Link href="/" className="font-bold text-3xl block mb-2">
           Health Sign
         </Link>
-      </div>
-      <div className="text-sm font-bold">
-          {user ? (
-            <div className="flex items-center space-x-5">
-              <Link href="/conditions/create">
-                <div>記録する</div>
-              </Link>
+      
+        <div className="text-sm font-bold mt-5">
+            {user ? (
+              <div className="flex items-center space-x-5">
+                <Link href="/conditions/create">
+                  <div>記録する</div>
+                </Link>
 
-              <Link href="/settings/profile">
-                <div>設定</div>
-              </Link>
+                <Link href="/settings/profile">
+                  <div>設定</div>
+                </Link>
 
-              <div className="cursor-pointer" onClick={handleLogout}>
-                <LogOut className="h-5 w-5" />
+                <div className="cursor-pointer" onClick={handleLogout}>
+                  <LogOut className="h-5 w-5" />
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="flex items-center space-x-5">
-              <Link href="/auth/login">ログイン</Link>
-              <Link href="/auth/signup">サインアップ</Link>
-            </div>
-          )}
+            ) : (
+              <div className="flex items-center space-x-5">
+                <Link href="/auth/login">ログイン</Link>
+                <Link href="/auth/signup">サインアップ</Link>
+              </div>
+            )}
+          </div>
         </div>
     </header>
   )
