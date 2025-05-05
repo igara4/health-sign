6f13fb43bdf176da01b092f8812e238e402099e5
igara4 <fifty.storm.snowman@gmail.com>
2025-05-06 08:18:26 +0900

    fix responseの型にlog_idを追加

--- lib/types/response.ts
+++ lib/types/response.ts
@@ -2,6 +2,7 @@ export type Response = {
   question_id: string;
   answer: boolean;
   created_at: string;
+  log_id: string;
   question: {
     text: string;
     category: "good" | "warning" | "bad" | string;
