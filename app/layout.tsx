940445aec9059d03a942d4774befa56599f0ea14
igara4 <fifty.storm.snowman@gmail.com>
2025-07-28 07:52:26 +0900

    fix:toast位置をヘッダー依存を解消

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <Navigation user={user} />
         <ToastProvider />
+        <Navigation user={user} />
         <div className="flex flex-col min-h-screen">
           <main className="flex-1 container max-w-screen-sm mx-auto px-1 py-5">
             {children}
